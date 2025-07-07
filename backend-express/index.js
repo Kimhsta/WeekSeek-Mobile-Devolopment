b3ce579179e84379469d24abbce609128f662af1
Kiki Mahesta <147258845+Kimhsta@users.noreply.github.com>
2025-07-07 23:03:34 +0700

    Add body-parser middleware and update package dependencies; initialize Prisma schema

--- backend-express/index.js
+++ backend-express/index.js
@@ -7,8 +7,18 @@ const app = express()
 //import cors
 const cors = require('cors')
 
+//import body parser
+const bodyParser = require('body-parser')
+
 //use cors
 app.use(cors())
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 //define port
 const port = 3000;
 
