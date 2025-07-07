d22b63c94e5dc613b9dc12e28c4926be02f35faf
Kiki Mahesta <147258845+Kimhsta@users.noreply.github.com>
2025-07-07 11:24:51 +0700

    Initialize backend-express project with package.json and essential dependencies

---
+++ backend-express/index.js
@@ -0,0 +1,23 @@
+//import express
+const express = require('express')
+
+//init app
+const app = express()
+
+//import cors
+const cors = require('cors')
+
+//use cors
+app.use(cors())
+//define port
+const port = 3000;
+
+//route
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+//start server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
