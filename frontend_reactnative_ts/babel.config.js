74a907e5ddedbb1e31ad713bce312fec578acec3
Kiki Mahesta <squadkapten2@gmail.com>
2025-07-17 23:19:21 +0700

    mumet lee

--- frontend_reactnative_ts/babel.config.js
+++ frontend_reactnative_ts/babel.config.js
@@ -1,4 +1,5 @@
 module.exports = function (api) {
+  console.log("✅ Babel config loaded");
     api.cache(true);
     return {
       presets: ['babel-preset-expo'],
--- frontend_reactnative_ts/tailwind.config.js
+++ frontend_reactnative_ts/tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./App.{js,ts,jsx,tsx}",
-      "./screens/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}"
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
-  
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
